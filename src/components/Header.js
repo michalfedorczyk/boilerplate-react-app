b12cba2f7eea54bbcc0757053ec0220897885b92
Michal M. Fedorczyk <michalfedorczyk@gmail.com>
2020-11-08 08:42:59 +0100

    Push of the whole repo

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { connect } from "react-redux";
+import { startLogout } from "../actions/auth";
+
+export const Header = ({ startLogout }) => (
+  <header className="header">
+    <div className="content-container">
+      <div className="header__content">
+        <NavLink className="header__title" to="/dashboard">
+          <h1>Boilerplate</h1>
+        </NavLink>
+        <button className="button button--link" onClick={startLogout}>
+          Logout
+        </button>
+      </div>
+    </div>
+  </header>
+);
+
+const mapDispatchToProps = (dispatch) => ({
+  startLogout: () => dispatch(startLogout()),
+});
+
+export default connect(undefined, mapDispatchToProps)(Header);
---
+++ src/components/LoginPage.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { connect } from "react-redux";
+import { startLogin } from "../actions/auth";
+
+export const LoginPage = ({ startLogin }) => (
+  <div className="box-layout">
+    <div className="box-layout__box">
+      <h1 className="box-layout__title">
+        <i className="fas fa-money-bill-alt"> </i> Boilerplate React App
+      </h1>
+      <p>Tag line for app.</p>
+      <button className="button" onClick={startLogin}>
+        <i className="fab fa-google"></i> Login with Google
+      </button>
+    </div>
+  </div>
+);
+
+const mapDispatchToProps = (dispatch) => ({
+  startLogin: () => dispatch(startLogin()),
+});
+
+export default connect(undefined, mapDispatchToProps)(LoginPage);
